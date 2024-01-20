6200eebf9d7c9f3cf4c26fb644949666f16005bc
dheerajnandiraju@gmail.com <dheerajnandiraju@gmail.com>
2024-01-20 11:37:40 +0530

    uday

--- src/components/admin-components/SearchResultsList.jsx
+++ src/components/admin-components/SearchResultsList.jsx
@@ -5,7 +5,7 @@ const SearchResultsList = ({ results }) => {
   return (
     <div className="flex flex-col max-h-[5rem] rounded-md w-full bg-background-components overflow-y-scroll">
       {results.map((result, id) => (
-        <NavLink to="/some" key={id} className="cursor-pointer">
+        <NavLink to={result.path} key={id} className="cursor-pointer">
           {result.name}
         </NavLink>
       ))}
